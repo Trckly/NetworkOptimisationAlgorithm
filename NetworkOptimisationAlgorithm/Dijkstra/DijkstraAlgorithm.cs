4c2f038ae4ee85bc762077d0d05315eb936a19d4
KOMPUTER\Danatello <chotkiydanik@gmail.com>
2024-10-25 13:46:52 +0300

    Implemented test logic for Dijkstra

--- NetworkOptimisationAlgorithm/Dijkstra/DijkstraAlgorithm.cs
+++ NetworkOptimisationAlgorithm/Dijkstra/DijkstraAlgorithm.cs
@@ -1,6 +1,89 @@
+using System.Runtime.InteropServices.JavaScript;
+using System.Collections.Generic;
+
 namespace NetworkOptimisationAlgorithm.Dijkstra;
 
 public class DijkstraAlgorithm
 {
-    
+    private int [,] _weightMatrix;
+
+    private Dictionary<int, bool> _markedNodes;
+    private List<int> _weightArray;
+    private List<int> _tracebackArray;
+
+    public DijkstraAlgorithm(int[,] weightMatrix)
+    {
+        var nodesCount = weightMatrix.GetLength(0);
+        
+        _markedNodes = new Dictionary<int, bool>();
+        _weightArray = new List<int>();
+        _tracebackArray = new List<int>();
+        _weightMatrix = new int[nodesCount, nodesCount];
+        
+        // Here negative values are being changed into max values and written into _weightMatrix
+        // Also all extra arrays are being initialized with default values
+        for (var i = 0; i < nodesCount; i++)
+        {
+            _weightArray.Add(int.MaxValue);
+            _tracebackArray.Add(-1);
+            
+            for (var j = 0; j < nodesCount; j++)
+            {
+                _weightMatrix[i, j] = weightMatrix[i, j] < 0 ? int.MaxValue : weightMatrix[i, j];
+            }
+        }
+    }
+
+    public void Solve()
+    {
+        
+    }
+
+    private void FillPathTable()
+    {
+        if (_markedNodes.Count == 0)
+        {
+            _markedNodes.Add(0, false);
+            _weightArray[0] = 0;
+            _tracebackArray[0] = -1;
+        }
+    }
+
+    private void CalculatePath()
+    {
+        // Get min path value of unmarked elements
+        var min = int.MaxValue;
+        var minIndex = -1;
+        for (var i = 0; i < _weightArray.Count; i++)
+        {
+            if (_markedNodes.TryGetValue(i, out var isTraversed) && !isTraversed)
+            {
+                if (min <= _weightArray[i]) continue;
+                min = _weightArray[i];
+                minIndex = i;
+            }
+        }
+        
+        for (var i = 0; i < _weightArray.Count; i++)
+        {
+            if (_markedNodes.TryGetValue(i, out var isTraversed) && !isTraversed && i != minIndex)
+            {
+                var weight = Dijkstra(minIndex, i);
+                if (weight != _weightArray[i])
+                {
+                    _weightArray[i] = weight;
+                    _tracebackArray[i] = minIndex;
+                }
+            }
+            else
+            {
+                
+            }
+        }
+    }
+
+    private int Dijkstra(int minIndex, int targetIndex)
+    {
+        return Math.Min(_weightArray[targetIndex], _weightArray[minIndex] + _weightMatrix[minIndex, targetIndex]);
+    }
 }
--- NetworkOptimisationAlgorithm/MainWindow.xaml.cs
+++ NetworkOptimisationAlgorithm/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,10 +15,163 @@ namespace NetworkOptimisationAlgorithm;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
+
 public partial class MainWindow : Window
 {
+    private int _nodeCount;
+
+    public int NodeCount
+    {
+        get => _nodeCount;
+        set => _nodeCount = value > 2 ? value : 2;
+    }
+
     public MainWindow()
     {
+        NodeCount = 9;
+        
         InitializeComponent();
+        GenerateTransportGrid();
+        PreDefine();
+    }
+    
+    private void GenerateTransportGrid()
+    {
+        TransportGrid ??= new Grid();
+        TransportGrid.RowDefinitions.Clear();
+        TransportGrid.ColumnDefinitions.Clear();
+        TransportGrid.Children.Clear();
+    
+        // Create Row Definitions
+        for (var i = 0; i <= NodeCount; i++)
+        {
+            TransportGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            TransportGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+        }
+    
+        // Create headers (top row and left column)
+        for (var i = 1; i <= NodeCount; i++)
+        {
+            var rowHeaders = new TextBlock
+            {
+                Text = ((char)('A' + i - 1)).ToString(),
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            Grid.SetRow(rowHeaders, i);
+            Grid.SetColumn(rowHeaders, 0);
+            TransportGrid.Children.Add(rowHeaders);
+            
+            var columnHeaders = new TextBlock
+            {
+                Text = ((char)('A' + i - 1)).ToString(),
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            Grid.SetRow(columnHeaders, 0);
+            Grid.SetColumn(columnHeaders, i);
+            TransportGrid.Children.Add(columnHeaders);
+        }
+    
+        // Fill grid cells with TextBoxes for input (except the first row/column for headers)
+        for (var i = 1; i <= NodeCount; i++)
+        {
+            for (var j = 1; j <= NodeCount; j++)
+            {
+                var cell = new TextBox
+                {
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    MinWidth = 30,
+                    MinHeight = 30,
+                };
+    
+                // Attach the validation event handler to each TextBox
+                cell.PreviewTextInput += Cell_PreviewTextInput;
+    
+                Grid.SetRow(cell, i);
+                Grid.SetColumn(cell, j);
+                TransportGrid.Children.Add(cell);
+            }
+        }
+    }
+    
+    private void PreDefine()
+    {
+        int [,]weightMatrix = new [,]
+        {
+            {0, 7, 9, 4, -1, -1, -1, -1, -1},
+            {7, 0, -1, 8, 1, -1, -1, 6, -1},
+            {9, -1, 0, 5, -1, 8, 4, -1, -1},
+            {4, 8, 5, 0, -1, -1, 9, 5, -1},
+            {-1, 1, -1, -1, 0, -1, -1, 2, -1},
+            {-1, -1, 8, -1, -1, 0, -1, -1, 3},
+            {-1, -1, 4, 9, -1, -1, 0, 7, 6},
+            {-1, 6, -1, 5, 2, -1, 7, 0, 8},
+            {-1, -1, -1, -1, -1, 3, 6, 8, 0},
+        };
+        
+        for (int i = 1; i <= NodeCount; i++)
+        {
+            for (int j = 1; j <= NodeCount; j++)
+            {
+                ((TextBox)GetGridElement(TransportGrid, i, j)).Text = weightMatrix[i - 1, j - 1].ToString();
+            }
+        }
+    }
+    
+    private UIElement GetGridElement(Grid grid, int row, int column)
+    {
+        foreach (UIElement element in grid.Children)
+        {
+            if (Grid.GetRow(element) == row && Grid.GetColumn(element) == column)
+                return element;
+        }
+
+        return null;
+    }
+    
+    // Event handler for TextBox validation: allows only numeric input
+    private void Cell_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+    {
+        Regex regex = new Regex("[^-?0-9]+"); // Only positive and negative digits are allowed
+        e.Handled = regex.IsMatch(e.Text);
+    }
+
+    private void PlusButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        NodeCount++;
+        GenerateTransportGrid();
+    }
+
+    private void SolveButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        // Read weight matrix from the grid
+        var weightMatrix = new int[NodeCount, NodeCount];
+        for (var i = 1; i <= NodeCount; i++)
+        {
+            for (var j = 1; j <= NodeCount; j++)
+            {
+                var cell = (TextBox)TransportGrid.Children
+                    .Cast<UIElement>()
+                    .FirstOrDefault(element => Grid.GetRow(element) == i && Grid.GetColumn(element) == j)!;
+                weightMatrix[i - 1, j - 1] = int.Parse(cell?.Text ?? string.Empty);
+            }
+        }
+
+        if ((string)AlgorithmComboBox.SelectedValue == "Dijkstra")
+        {
+            Console.WriteLine("Dijkstra");
+        }
+        else
+        {
+            Console.WriteLine("Floyd");
+        }
+    }
+
+    private void MinusButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        NodeCount--;
+        GenerateTransportGrid();
     }
 }
